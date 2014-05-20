e6486004b4283b27da770a7168fe0422dcf3275f
soramusoka <soramusoka@gmail.com>
2014-05-20 21:27:56 +0400

    added ORF task

---
+++ 17-open-reading-frames.py
@@ -0,0 +1,38 @@
+import math
+import ext
+
+n = 3
+finalResult = {}
+
+fasta = ext.Fasta().fromFile('rosalind_orf.txt');
+key = fasta.getKeys()[0]
+source = fasta.data[key]
+
+revSource = ext.rev_and_conv(source)
+
+def find_orf(resDic, source):
+	for i in range(0, len(source)):
+		data = ext.splitBy(source[i:], n)
+		if(len(data) > 1):
+			flagStart = False
+			flagStop = False
+			sequence = ''
+			for elm in data:
+				if(ext.isStartCodon(elm) and flagStart == False):
+					flagStart = True
+
+				flagStop = ext.isStopCodon(elm)
+				if(flagStop):
+					break
+
+				if(flagStart and len(elm) == 3):
+					sequence += ext.dna_codon_table[elm]
+
+			if(len(sequence) > 0 and flagStop):	
+				resDic[sequence] = 1				
+
+find_orf(finalResult, source)
+find_orf(finalResult, revSource)
+
+for i in finalResult:
+	print i
--- ext.py
+++ ext.py
@@ -60,16 +60,34 @@ def read_file(file_name):
 
 
 # ========================================================================
-rna_dic = {}
+rna_codon_table = {}
 rna_source = read_file('rna-codon-table.txt')
 for val in rna_source:
 	val = val.replace('\n', '')
 	data = val.split(' ')
-	rna_dic[data[0]] = data[1]
+	rna_codon_table[data[0]] = data[1]
 
 def rna_value_by_key(value):
-	return rna_dic[value]
+	return rna_codon_table[value]
 
+dna_codon_table = {
+	'TTT': 'F', 'CTT': 'L', 'ATT': 'I', 'GTT': 'V',
+	'TTC': 'F', 'CTC': 'L', 'ATC': 'I', 'GTC': 'V',
+	'TTA': 'L', 'CTA': 'L', 'ATA': 'I', 'GTA': 'V',
+	'TTG': 'L', 'CTG': 'L', 'ATG': 'M', 'GTG': 'V',
+	'TCT': 'S', 'CCT': 'P', 'ACT': 'T', 'GCT': 'A',
+	'TCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+	'TCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+	'TCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+	'TAT': 'Y', 'CAT': 'H', 'AAT': 'N', 'GAT': 'D',
+	'TAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+	'TAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+	'TAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+	'TGT': 'C', 'CGT': 'R', 'AGT': 'S', 'GGT': 'G',
+	'TGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+	'TGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+	'TGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'
+}
 
 # ========================================================================
 mono_mass_table = {}
@@ -95,3 +113,27 @@ def Fib(n):
     if n == 0: return 0
     elif n == 1: return 1
     else: return Fib(n-1)+Fib(n-2)
+
+def splitBy(source, n):
+	return [source[i:i+n] for i in range(0, len(source), n)]
+
+def rev_and_conv(source):
+	source = source[::-1]
+	result = ''
+	for char in source:
+		if(char == 'T'):
+			result += 'A'
+		elif(char == 'A'):
+			result += 'T'
+		elif(char == 'C'):
+			result += 'G'
+		elif(char == 'G'):
+			result += 'C'
+
+	return result
+
+def isStartCodon(elm):
+	return elm == 'ATG' or elm == 'AUG'
+
+def isStopCodon(elm):
+	return elm == 'TAA' or elm == 'TGA' or elm == 'TAG' or elm == 'UAA' or elm == 'UGA' or elm == 'UAG'
